2af3a679eac0ebd90f35b7c784aa77bd97f21aef
Kyle McParland <kylemcparland@gmail.com>
2024-07-09 21:45:11 -0400

    implemented wordsearch.js in pair programming session

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    const columnLength = matrix.length;
+    const rowsLength = matrix[0].length;
+    //empty matrix init
+    let newMatrix = [];
+    //create an empty array for each column in the original matrix
+    for (let i = 0; i < rowsLength; i++) {
+      newMatrix[i] = [];
+      // add the relative part of the original matrix
+      for (let j = 0; j < columnLength; j++) {
+        newMatrix[i][j] = matrix[j][i];
+      }
     }
-}
-
-module.exports = wordSearch
+    return newMatrix;
+  };
+  
+  const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    const lettersTransposed = transpose(letters);
+    const verticalJoin = lettersTransposed.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  }
+  
+  module.exports = wordSearch
